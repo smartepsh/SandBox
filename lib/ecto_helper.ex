512ceb4339b88345fd6907091349a4d2fb828895
Kenton Wang <smartepsh@gmail.com>
2019-03-12 16:25:50 +0800

    feat: add ecto helper - set_position/2

--- lib/ecto_helper.ex
+++ lib/ecto_helper.ex
@@ -1,2 +1,24 @@
 defmodule Sandbox.EctoHelpers do
+  import Ecto.Changeset
+
+  def set_position(%{valid?: true, changes: changes} = changeset, key) do
+    case get_change(changeset, key) do
+      nil ->
+        changeset
+
+      item_chsets ->
+        {replace_chsets, update_chsets} = Enum.split_with(item_chsets, &(&1.action == :replace))
+
+        update_chsets =
+          update_chsets
+          |> Enum.with_index(1)
+          |> Enum.map(fn {chset, pos} ->
+            put_change(chset, :pos, pos)
+          end)
+
+        %{changeset | changes: Map.put(changes, key, replace_chsets ++ update_chsets)}
+    end
+  end
+
+  def set_position(changeset, _key), do: changeset
 end
